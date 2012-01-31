4739966cfd882132445791bfa9ea8bc6310e5c02
Nan Liu <nan@puppetlabs.com>
2012-01-31 15:37:50 -0500

    Fix dynamic scope variables.

--- lib/puppet/parser/functions/ss_passwd_age.rb
+++ lib/puppet/parser/functions/ss_passwd_age.rb
@@ -1,7 +1,7 @@
 # Return the age of the password for the specified user, using the custom facts
 module Puppet::Parser::Functions
   newfunction(:ss_passwd_age, :type=>:rvalue) do |args|
-    value = lookupvar("ss_passwd_age_"+args[0]).to_i
+    value = lookupvar("::ss_passwd_age_"+args[0]).to_i
     value ||= -1
   end
 end
